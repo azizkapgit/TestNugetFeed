bf44109d67747103d34104f432e6effe1fdab4a9
azizkapgit <azizkap.git@gmail.com>
2016-07-11 23:11:03 +0100

    Commit project

---
+++ Project/TestNugetFeed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestPackageManager.Common;
+
+namespace TestNugetFeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string testSettings = Utility.GetAppSetting("test", "");
+            
+        }
+    }
+}
